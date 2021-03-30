8d9e4c2af63cc1d2ef7ffb551309352b7eee8268
keremsabirli <keremsabirli13@gmail.com>
2021-03-30 22:08:02 +0300

    added files

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b202ee1ca1842fa7bf5b58248f58fa72'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '092f2ef4-2f18-4df9-89f1-e28957e0fb1e'
+};
